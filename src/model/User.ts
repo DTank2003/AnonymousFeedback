ddf36df059d245df586e27bedbbacd74bd949753
DTank2003 <dhyeyworkid1805@gmail.com>
2024-08-18 23:47:57 +0530

    DB setup

--- src/model/User.ts
+++ src/model/User.ts
@@ -41,7 +41,7 @@ const UserSchema: Schema<User> = new Schema({
         type: String,
         required: [true, "Email is required"],
         unique: true,
-        match: [/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, 'Please use a valid email address']
+        match: [/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, "Please use a valid email address"]
     },
     password: {
         type: String,
